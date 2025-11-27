940d07d0da504184f52c19675d4e020bc8f2c7a5
ctp1299 <filip.wagner@takeda.com>
2025-11-27 10:06:15 +0100

    Added sub-folder-based logging

--- api_ingestion/job_logger.py
+++ api_ingestion/job_logger.py
@@ -16,6 +16,9 @@ def _normalize_dbfs_path(path: str) -> str:
 
 def init_job_logger(volume_path: str, task_name: str, identity_suffix: str | None = None) -> tuple[logging.Logger, str]:
     local_volume = _normalize_dbfs_path(volume_path)
+    # create job-specific sub-folder for logs
+    folder_name = f"job_{datetime.now().strftime('%Y-%m-%d_%H')}_{task_name}/"
+    local_volume = f"{local_volume}/{folder_name}"
     os.makedirs(local_volume, exist_ok=True)
 
     if not identity_suffix:
@@ -23,10 +26,8 @@ def init_job_logger(volume_path: str, task_name: str, identity_suffix: str | Non
             os.environ.get("SPARK_EXECUTOR_ID")
             or f"{socket.gethostname()}_{os.getpid()}"
         )
-    folder_name = f"job_{datetime.now().strftime('%Y-%m-%d_%H')}_{task_name}/"
     filename = f"job_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')[:-3]}_{task_name}_{identity_suffix}.log"
-    #create job-specific sub-folder for logs
-    log_file = os.path.join(local_volume,folder_name, filename)
+    log_file = os.path.join(local_volume, filename)
 
     logger_name = f"api_ingestion.{task_name}.{identity_suffix}"
     logger = logging.getLogger(logger_name)
