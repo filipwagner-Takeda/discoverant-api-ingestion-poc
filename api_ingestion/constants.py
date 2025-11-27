b7c34a98d22e72fb3629bdec0ebd3e818aeeef51
ctp1299 <filip.wagner@takeda.com>
2025-11-27 12:07:56 +0100

    Remove useless constants

--- api_ingestion/constants.py
+++ api_ingestion/constants.py
@@ -1,10 +1,7 @@
 #TODO cleanup later, some of these are not used/shouldn't be used
-USERNAME:str = ""
-PASSWORD: str = ""
 THROTTLE: float = 1
 RETRIES: int = 2
 BACKOFF_FACTOR = 2
 MAX_PAGES = 50
 CHUNK_SIZE = 10
-REQUIRED_KEYS_CONFIG = ['base-url', 'endpoint']
 SUPPORTED_PARTITION_STRATEGIES = ['','page','param']
